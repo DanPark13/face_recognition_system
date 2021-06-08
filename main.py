394f522e2669169d5fa524fc6129a34f23854db3
Daniel Park <dpark0703@gmail.com>
2021-06-08 10:47:54 -0500

    Add Face Detection Functionality
    - Implemented using Face Recognition Library
    - Gets location and encodes face
    - Draw a rectangle around the face

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+'''
+Prototype Facial Recognition Testing
+'''
+
+# Import Libraries
 import cv2
 import numpy as np
 import face_recognition
@@ -6,16 +11,42 @@ import face_recognition
 Load images for testing
 '''
 # Load training image
-training_image_trump = face_recognition.load_image_file("training_images/donald_trump_official.jpg")
+training_image_trump = face_recognition.load_image_file("training_images/donald_trump_official.jpg.jpg")
 # Convert training image to RGB (red-green-blue)
 training_image_trump = cv2.cvtColor(training_image_trump, cv2.COLOR_BGR2RGB)
 # Load testing image
-testing_image_trump = face_recognition.load_image_file("testing_images/donald_trump_test.jpg")
+testing_image_trump = face_recognition.load_image_file("testing_images/donald_trump_test.jpg.jpg")
 # Convert testing image to RGB (red-green-blue)
 testing_image_trump = cv2.cvtColor(testing_image_trump, cv2.COLOR_BGR2RGB)
 
 '''
-Show Trump Images
+Detect Training Face
+'''
+# Identify the location of the Face
+trump_train_face_location = face_recognition.face_locations(training_image_trump)[0]
+# Encodes the location of the Face (used for comparison)
+encode_trump_train_face = face_recognition.face_encodings(training_image_trump)[0]
+# Draw a rectangle around the Face using the Face Location
+cv2.rectangle(training_image_trump, (trump_train_face_location[3], trump_train_face_location[0]),
+              (trump_train_face_location[1], trump_train_face_location[2]), (255, 0, 255), 2)
+
+print(f"Training Trump Face Location: ", trump_train_face_location)
+
+'''
+Detect Testing Face
+'''
+# Identify the location of the Face
+trump_test_face_location = face_recognition.face_locations(testing_image_trump)[0]
+# Encodes the location of the Face (used for comparison)
+encode_trump_test_face = face_recognition.face_encodings(testing_image_trump)[0]
+# Draw a rectangle around the Face using the Face Location
+cv2.rectangle(testing_image_trump, (trump_test_face_location[3], trump_test_face_location[0]),
+              (trump_test_face_location[1], trump_test_face_location[2]), (255, 0, 255), 2)
+
+print(f"Testing Trump Face Location: ", trump_test_face_location)
+
+'''
+Show Testing Images
 '''
 cv2.imshow("Donald Trump Official", training_image_trump)
 cv2.imshow("Donald Trump Test", testing_image_trump)

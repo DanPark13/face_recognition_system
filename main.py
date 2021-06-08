13095c809fb12665e8102cf7b6eb3f53a468159d
Daniel Park <dpark0703@gmail.com>
2021-06-08 10:11:11 -0500

    Preliminaries to Get the Model Up for Training
    - Add Training/Testing Images
    - Load Images onto the Project
    - Show Images when Project is run

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import cv2
 import numpy as np
 import face_recognition
+
+'''
+Load images for testing
+'''
+# Load training image
+training_image_trump = face_recognition.load_image_file("training_images/donald_trump_official.jpg")
+# Convert training image to RGB (red-green-blue)
+training_image_trump = cv2.cvtColor(training_image_trump, cv2.COLOR_BGR2RGB)
+# Load testing image
+testing_image_trump = face_recognition.load_image_file("testing_images/donald_trump_test.jpg")
+# Convert testing image to RGB (red-green-blue)
+testing_image_trump = cv2.cvtColor(testing_image_trump, cv2.COLOR_BGR2RGB)
+
+'''
+Show Trump Images
+'''
+cv2.imshow("Donald Trump Official", training_image_trump)
+cv2.imshow("Donald Trump Test", testing_image_trump)
+cv2.waitKey(0)
